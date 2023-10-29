c4895b117dfc7dcc58414f7f738ac47b4d47f078
Yulia Okhotina <ailushka@MacBook-Pro-Yulia.local>
2023-10-29 03:48:05 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+/* -------------------- */
+/*         Popup        */
+/* -------------------- */
+
+const aboutButtons = document.querySelectorAll('.button_type_about');
+const closeButtons = document.querySelectorAll('.button_type_close');
+
+const ESCAPE = 27;
+
+function openPopUp(popup) {
+  popup.classList.add('popup_opened');
+  popup.classList.add('transition');
+  document.querySelector('.page').classList.add('no-scroll');
+  document.addEventListener('click', closePopUpByOverlay);
+  document.addEventListener('keydown', closePopUpByEsc);
+
+}
+
+function closePopUp(popup) {
+  popup.classList.remove('popup_opened');
+  popup.classList.remove('transition');
+  document.querySelector('.page').classList.remove('no-scroll');
+  document.removeEventListener('click', closePopUpByOverlay);
+  document.removeEventListener('keydown', closePopUpByEsc);
+}
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+function closePopUpByOverlay(evt) {
+  if (evt.target.classList.contains('popup_opened')) {
+    closePopUp(evt.target);
+  }
+}
+
+function closePopUpByEsc(evt) {
+  const popupOpened = document.querySelector('.popup_opened');
+  if (evt.keyCode === ESCAPE) {
+    closePopUp(popupOpened);
+  }
+}
+
+aboutButtons.forEach((item) => {
+  item.addEventListener('click', () => {
+    const targetPopupId = item.getAttribute('aria-labelledby');
+    const targetPopup = document.querySelector(`#${targetPopupId}`);
+
+    openPopUp(targetPopup);
+
+  });
+});
+
+closeButtons.forEach((item) => {
+  item.addEventListener('click', (evt) => {
+    const popUpToClose = evt.target.closest('.popup');
+    closePopUp(popUpToClose);
+  });
+});
